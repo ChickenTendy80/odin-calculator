7d32e4a68acc3be710d32d1bb67ee8adc0fdef14
Ryan Kim <ryanhkim0418@gmail.com>
2025-06-19 21:21:17 -0500

    completed implementation of operations - need to work on style

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let var1 ="";
 let var2 = "";
+const integers = "1234567890";
 let operation = "";
 let output ="";
 
@@ -9,13 +10,52 @@ const results = document.querySelector(".result");
 button.forEach(elt => {
     elt.addEventListener("click", () => {
         //Add text to results
-        output += elt.value;
-        results.textContent = output;
-        if(elt.value == "="){
-            //Call operation to the output
+        if(elt.value == "cl"){
             output = "";
+            //console.log("empty");
+        }
+        else{
+            output += elt.value;
+            //console.log("keep going!");
+        }
+        results.textContent = output;
+
+        if(elt.value == "="){
+            //console.log("equals!");
+            //Call operation to the output
+            let strArray = [];
+            let numberArray =[];
+            output = output.slice(0,-1);
+
+            if(output.includes("+")){
+                strArray = output.split("+");
+                numberArray = strArray.map((str)=>+str);
+                //console.log(numberArray);
+                output = operate("+",numberArray[0],numberArray[1]);
+                //console.log(output);
+            }
+            else if(output.includes("-")){
+                strArray = output.split("-");
+                numberArray = strArray.map((str)=>+str);
+                //console.log(numberArray);
+                output = operate("-",numberArray[0],numberArray[1]);
+            }
+            else if(output.includes("*")){
+                strArray = output.split("*");
+                numberArray = strArray.map((str)=>+str);
+                //console.log(numberArray);
+                output = operate("*",numberArray[0],numberArray[1]);
+            }
+            else if(output.includes("/")){
+                strArray = output.split("/");
+                numberArray = strArray.map((str)=>+str);
+                //console.log(numberArray);
+                output = operate("/",numberArray[0],numberArray[1]);
+            }
+
             //Show output in result
-            results.textContent = output;
+            //console.log(output);
+            results.textContent = output + "";
         }
     });
 });
@@ -28,6 +68,7 @@ button.forEach(elt => {
 
 /*Basic operations--------------------------------------------------*/
 function add(a,b){
+    //console.log("addition done!");
     return a+b;
 }
 
@@ -46,17 +87,18 @@ function divide(a,b){
 
 /*Calling the operations---------------------------------------------*/
 function operate(str,a,b){
-    if(str=="add"){
-        add(a,b);
+    if(str=="+"){
+        return add(a,b);
+        //console.log("adding right now");
     }
-    else if(str=="subtract"){
-        subtract(a,b);
+    else if(str=="-"){
+        return subtract(a,b);
     }
-    else if(str=="multiply"){
-        multiply(a,b);
+    else if(str=="*"){
+        return multiply(a,b);
     }
-    else if(str=="divide"){
-        divide(a,b);
+    else if(str=="/"){
+        return divide(a,b);
     }
     else{
         return "INVALID OPERATION";
